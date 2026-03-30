b3785ff9396f489b7c9f265e1a12b7477efc682f
Mswati-tz <132760726+Mswati-tz@users.noreply.github.com>
2026-03-30 15:31:30 +0300

    Update serializers.py

--- DT1/serializers.py
+++ DT1/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
-from .models import Askari, Report, Supervisor
+from .models import Askari, Report, ContactMessage
 
 class AskariSerializer(serializers.ModelSerializer):
-    # Hapa tunachukua jina la supervisor badala ya ID tu
-    supervisor_name = serializers.ReadOnlyField(source='supervisor.full_name')
-    site_name = serializers.ReadOnlyField(source='current_site.site_name')
-
     class Meta:
         model = Askari
-        fields = ['id', 'full_name', 'badge_number', 'phone_number', 'supervisor_name', 'site_name']
+        fields = '__all__'
 
 class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
+        fields = '__all__'
+
+class ContactMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactMessage
         fields = '__all__'
