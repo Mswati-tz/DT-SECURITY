d8e71c625c6810e84f4b07cf6e1e8236a2f44837
Mswati-tz <132760726+Mswati-tz@users.noreply.github.com>
2026-03-30 17:16:22 +0300

    Update serializers.py

--- DT1/serializers.py
+++ DT1/serializers.py
@@ -1,17 +1,1 @@
-from rest_framework import serializers
-from .models import Askari, Report, ContactMessage
 
-class AskariSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Askari
-        fields = '__all__'
-
-class ReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Report
-        fields = '__all__'
-
-class ContactMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactMessage
-        fields = '__all__'
