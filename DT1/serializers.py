80d9e7a9091d5e2401ae2f4c49d6a0d7b08ac17c
Mswati-tz <132760726+Mswati-tz@users.noreply.github.com>
2026-03-30 15:28:58 +0300

    Update serializers.py

--- DT1/serializers.py
+++ DT1/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Askari, Report, Supervisor
 
-class StudentSerializer(serializers.ModelSerializer):
+class AskariSerializer(serializers.ModelSerializer):
+    # Hapa tunachukua jina la supervisor badala ya ID tu
+    supervisor_name = serializers.ReadOnlyField(source='supervisor.full_name')
+    site_name = serializers.ReadOnlyField(source='current_site.site_name')
+
     class Meta:
-        model = Student
-        fields = ['id', 'first_name', 'last_name', 'reg_number', 'course']
+        model = Askari
+        fields = ['id', 'full_name', 'badge_number', 'phone_number', 'supervisor_name', 'site_name']
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = '__all__'
